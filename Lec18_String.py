0b8082569402dd3d36fa1a4f1b806d23a963b39a
Milankumarp <milankumarp@quickcap.net>
2025-08-21 13:01:24 +0530

    Add examples for string case manipulation and unique IP extraction in Lec18_String.py

--- Lec18_String.py
+++ Lec18_String.py
@@ -1,5 +1,5 @@
 #String in python is case sensitive
-
+'''
 Name = 'Milan Pansuriya'
 
 #Iteration in string
@@ -31,3 +31,57 @@ print(Name.isdigit())                 #Check if the string contains only digits 
 print(ord('a'))                       #Get the Unicode code point of 'a', ASCII value is 97, it give ascii value
 print(chr(97))                        #Get the character corresponding to the Unicode code point 97, it give 'a'
 print(Name.swapcase())                #Swap case of the string, it convert uppercase to lowercase and lowercase to uppercase
+
+'''
+
+'''
+Q1. Swap the case of the string without using swapcase 
+inbuilt method for string
+
+Input:- Programming Aasan Hai
+Output:- pROGRAMMING aASAN hAI 
+'''
+
+'''
+Q2. Print the list of all unique ip addresses?
+
+Input:- 
+"/region//us-east-a/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.155.2/file_path//usr/bin/test1.csv",
+"/region//us-east-b/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.156.2/file_path/teams/bin/test1.csv",
+"/region//us-east-c/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.151.2/file_path/teams/bin/test1.csv",
+"/region/japan/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.155.22/file_path/data/bin/test1.csv",
+"/region/india/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.167.2/file_path//usr/bin/test1.csv",
+"/region//us-east-a/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.179.28/file_path//usr/bin/test1.csv",
+"/region//us-east-b/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.155.31/file_path/worklog/bin/test1.csv",
+"/region//us-east-c/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.151.2/file_path//tmp/bin/test1.csv"
+]
+
+Output:- ["10.168.155.2","10.168.156.2","10.168.151.2"
+           "10.168.155.22","10.168.167.2",
+           "10.168.179.28","10.168.155.31" ]
+'''
+paths = """/region//us-east-a/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.155.2/file_path//usr/bin/test1.csv",
+    "/region//us-east-b/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.156.2/file_path/teams/bin/test1.csv",
+    "/region//us-east-c/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.151.2/file_path/teams/bin/test1.csv",
+    "/region/japan/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.155.22/file_path/data/bin/test1.csv",
+    "/region/india/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.167.2/file_path//usr/bin/test1.csv",
+    "/region//us-east-a/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.179.28/file_path//usr/bin/test1.csv",
+    "/region//us-east-b/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.155.31/file_path/worklog/bin/test1.csv",
+    "/region//us-east-c/north/resource/vminsatnce/subsid/ae-456-df/server/10.168.151.2/file_path//tmp/bin/test1.csv"""
+print(paths)
+print(len(paths))
+
+new_paths = paths.split("/")
+#print(new_paths)
+
+unique_ips = set()
+All_ips = []
+
+for i in new_paths:
+    if i.startswith("10."):
+        unique_ips.add(i)
+        All_ips.append(i)
+    
+print(unique_ips)
+print(All_ips)
+
