78aca221bf6927b191bbd0191f611048f05a3c2d
Milan Pansuriya <milanpansuriya1234@gmail.com>
2026-04-17 18:44:21 +0530

    Add initial variable definitions and print statements in Lec04_Variable.py

---
+++ Lec04_Variable.py
@@ -0,0 +1,10 @@
+length_of_land = 100
+bricks_cost_per_piece = 10.5
+labour_mistri1 = "Jagmohan"
+is_home = True
+
+print(length_of_land,bricks_cost_per_piece,labour_mistri1,is_home)
+print(type(length_of_land),type(bricks_cost_per_piece),type(labour_mistri1),type(is_home))
+
+# Python is Dynamically Typed language because you do not declare the type of variable while creating it. It automatically detects the type of variable based on the value assigned to it.
+
