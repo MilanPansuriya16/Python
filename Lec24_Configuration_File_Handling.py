e26083cef43d5d3fb1695373a04f7f0c7c78c6b2
Milan Pansuriya <milanpansuriya1234@gmail.com>
2026-04-30 12:19:45 +0530

    added Config file access

--- Lec24_Configuration_File_Handling.py
+++ Lec24_Configuration_File_Handling.py
@@ -37,3 +37,81 @@ result = total_cost_for_bricks(config)
 
 logger.info(f"Total bricks cost to make 1 room {result}")
 
+##############################################################################################
+'''
+2) Pragramming Question:
+
+A) Calculate the cost of boeks bouget by each Student
+
+book_name   :cost
+
+Science 	: 262
+moth        : 178
+History     : 59
+Ph&ics      : 231
+Biologa     : 165
+chemistry   : 110
+
+student_details = {1:["Math","History"],2:["Biology","Chemistry","History"],3:["Science"]}
+
+condition: book cost detail in the ini file
+'''
+
+from loguru import logger
+import configparser
+
+config = configparser.ConfigParser()
+
+config.read(r"C:\Users\milan.pansuriya\Documents\Python\book_config_file.ini")
+
+# Math_Book_Cost = config["Book"]["Math"]
+# print(Math_Book_Cost)
+
+student_details = {1:["Math","History"],2:["Biology","Chemistry","History"],3:["Science"]}
+
+student_book_cost = {}
+for key,values in student_details.items():
+    book_price = 0
+    for i in values:
+        book_price = book_price + int(config["Book"][i])
+    student_book_cost[key] = book_price
+    
+print(student_book_cost)
+
+##############################################################################################
+'''
+B) What will be the sot for books if discount 10%.
+but the condition to get discount is,  you will have to buy 2 or more books.
+'''
+
+
+from loguru import logger
+import configparser
+
+config = configparser.ConfigParser()
+
+config.read(r"C:\Users\milan.pansuriya\Documents\Python\book_config_file.ini")
+
+# Math_Book_Cost = config["Book"]["Math"]
+# print(Math_Book_Cost)
+
+student_details = {1:["Math","History"],2:["Biology","Chemistry","History"],3:["Science"]}
+
+student_book_cost = {}
+for key,values in student_details.items():
+    book_price = 0
+    count = 0 
+    for i in values:
+        count += 1
+        book_price = book_price + int(config["Book"][i])
+    
+    if count>1:
+        book_price = book_price - (book_price * 0.1)
+    student_book_cost[key] = book_price
+    
+print(student_book_cost)
+
+
+# print((178 + 59)- ((178 + 59)*0.1))
+# print((165 + 110 + 59)- ((165 + 110 + 59)*0.1))
+# print(262)
--- New Test file for Python Code.py
+++ New Test file for Python Code.py
@@ -1,24 +1,31 @@
+
 from loguru import logger
+import configparser
 
-def final_cart_amount (*args,discount = 0.1):
-    try:
-        result = 0
-        for amount in args:
-            result = result + amount
+config = configparser.ConfigParser()
 
-        amount_after_discount = result - (result * discount)
+config.read(r"C:\Users\milan.pansuriya\Documents\Python\book_config_file.ini")
 
-        return amount_after_discount
-    except TypeError:
-        logger.info("Please provide the amount in integer")
-        raise Exception("Value provided is not an interger coming from Type Error")
-    except Exception:
-        logger.info("Can not process the cart amount")
-        raise 
+# Math_Book_Cost = config["Book"]["Math"]
+# print(Math_Book_Cost)
 
-try:
-    final_amount_to_be_paid = final_cart_amount(100,200,300,400,"500",discount = 0.5)
-    logger.info(f"final amount to be paid = {final_amount_to_be_paid}")
-except Exception as e:
-    logger.info(e)
+student_details = {1:["Math","History"],2:["Biology","Chemistry","History"],3:["Science"]}
 
+student_book_cost = {}
+for key,values in student_details.items():
+    book_price = 0
+    count = 0 
+    for i in values:
+        count += 1
+        book_price = book_price + int(config["Book"][i])
+    
+    if count>1:
+        book_price = book_price - (book_price * 0.1)
+    student_book_cost[key] = book_price
+    
+print(student_book_cost)
+
+
+# print((178 + 59)- ((178 + 59)*0.1))
+# print((165 + 110 + 59)- ((165 + 110 + 59)*0.1))
+# print(262)
