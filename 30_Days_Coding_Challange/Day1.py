216a0326cd14a45415a50c9687a7c4c27f4c1bb9
Milan Pansuriya <milanpansuriya1234@gmail.com>
2026-06-02 16:18:59 +0530

    Day1 of Python Coding

---
+++ 30_Days_Coding_Challange/Day1.py
@@ -0,0 +1,5 @@
+# Day 1: Print Alternate Elements of an Array and Count Odd Even
+'''
+https://www.geeksforgeeks.org/problems/print-alternate-elements-of-an-array/1?page=1&category=Arrays&difficulty=Basic&sortBy=submissions
+https://www.geeksforgeeks.org/problems/count-odd-even/1
+'''
--- New Test file for Python Code.py
+++ New Test file for Python Code.py
@@ -1,2 +1,12 @@
 
 
+class Student:
+
+    def __init__(self, name):
+        self.name = name      # instance variable
+
+s1 = Student("Rahul")
+s2 = Student("Amit")
+
+print(s1.name)
+print(s2.name)
