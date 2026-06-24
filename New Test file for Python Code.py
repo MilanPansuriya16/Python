cebad8596006988802006056401943078111993a
Milan Pansuriya <milanpansuriya1234@gmail.com>
2026-06-24 19:08:51 +0530

    test python code

--- New Test file for Python Code.py
+++ New Test file for Python Code.py
@@ -1,13 +1,24 @@
 
-arr = [2,3,2,3,5]
-dict = {}
 
-for i in range(1,len(arr)+1):
-    dict[i] = 0
 
-for i in arr:
-    if i in dict:
-        dict[i] +=1
-    
-lst = list(dict.values())
-print(lst)
+def firstRepeated(arr):
+        # code here 
+        
+        dict = {}
+        repeat_element = -1
+        
+        for i in range(len(arr)):
+            if arr[i] in dict:
+                repeat_element = arr[i]
+            else:
+                dict[arr[i]] = 1
+            
+        for i in range(len(arr)):
+            if arr[i] == repeat_element:
+                return i
+        
+        return -1
+
+arr = [1,5,3,4,3,5,6]
+print(firstRepeated(arr))
+
