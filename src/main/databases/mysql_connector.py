e0eb3721db60b015a93d6e10ad263ffb6fc43a33
MilanPansuriya16 <milanpansuriya1234@gmail.com>
2026-06-28 16:27:25 +0530

    Implement MySqlConnection and MySqlCURDOperation classes for database interaction

--- src/main/databases/mysql_connector.py
+++ src/main/databases/mysql_connector.py
@@ -2,37 +2,47 @@ import mysql.connector
 from src.main.encrypt_decrypt.Lec25_AES_encryption import decrypt
 from loguru import logger
 
-
-def read_from_mysql(config,query):
-
-    connection = None
-    cursor = None
-    try:
-        connection = mysql.connector.connect(host = config["mysql_database"]["host"],
-                                            user = config["mysql_database"]["user"],
-                                            password = config["mysql_database"]["password"],
-                                            database = config["mysql_database"]["databases"])
-
-        # logger.info(f"{connection}")      # To check the connection is established or not
-
-
-        # read the data from databases
-        # query = 'select * from labours_table'
-        cursor = connection.cursor()
-        cursor.execute(query)
-        result = cursor.fetchall()
-        # logger.info(f"{result}")
-        # logger.info('Query done in the database')
-        return result
+class MySqlConnection:
+    def __init__(self,config):
+        self.config = config
+        self.connection = None
     
-    except Exception as e:
-        logger.info(f"Error occured in mysql as {e}")
-        raise e
+    def connect(self):
+        try:
+            self.connection = mysql.connector.connect(host = self.config["mysql_database"]["host"],
+                                                user = self.config["mysql_database"]["user"],
+                                                password = self.config["mysql_database"]["password"],
+                                                database = self.config["mysql_database"]["databases"])
+            logger.info(f"MySql Connection succesful")
+        except Exception as e:
+            logger.error(f"Error Occured: {e}")
+            raise e
+        
 
-    finally:
-        # cursor.close()
-        # connection.close()
-        if cursor is not None:
-            cursor.close()
-        if connection is not None and connection.is_connected():
-            connection.close()
+    def close(self):
+        if self.connection.is_connected():
+            self.connection.close()
+            logger.info(f"MySql Connection Close")
+
+
+class MySqlCURDOperation:
+    def __init__(self,mysql_connection):
+        self.connection = mysql_connection
+
+    def read_from_mysql(self,query):
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(query)
+            result =  cursor.fetchall()
+            return result
+        except Exception as e:
+            logger.info(f"Error in MySql Query Run {e}")
+            raise e
+        finally:
+            if cursor:
+                cursor.close()
+                logger.info("cursor closed")
+
+
+        
+        
--- src/main/main.py
+++ src/main/main.py
@@ -5,11 +5,15 @@ config = configparser.ConfigParser()
 config.read(r"src\resources\config_file.ini")
 
 def main():
-    query = 'select * from labours_table'
-    print(config.sections())
-    final_result = read_from_mysql(config, query)
-    logger.info(f"{final_result}")
+    my_sql_db_connection_obj = MySqlConnection(config)
+    my_sql_db_connection_obj.connect()
 
+    query = 'select * from labours_table'
+    crud_operation_obj = MySqlCURDOperation(my_sql_db_connection_obj.connection)
+    final_result = crud_operation_obj.read_from_mysql(query)
+    logger.info(f"{final_result}")
+    my_sql_db_connection_obj.close()
+    
 
 if __name__ == "__main__":
     main()
