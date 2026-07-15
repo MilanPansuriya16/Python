d8366299b9ae0fe1cc3ae8cf2c6868f04965930c
Milan Pansuriya <milanpansuriya1234@gmail.com>
2026-07-15 16:00:44 +0530

    Add implementation for finding the union of two sorted arrays

---
+++ Python_Coding_Challange/Union of 2 Sorted Arrays.py
@@ -0,0 +1,42 @@
+'''
+https://www.geeksforgeeks.org/problems/union-of-two-sorted-arrays-1587115621/1
+'''
+
+
+#########################################################################################
+
+class Solution:
+    def findUnion(self, a, b):
+        # code here 
+        
+        n = len(a)
+        m = len(b)
+        
+        i = 0 
+        j = 0
+        
+        result = []
+        
+        while i < n and j < m:
+            if a[i] <= b[j]:
+                if len(result) == 0 or result[-1] != a[i]:
+                    result.append(a[i])
+                i += 1
+            else:
+                if len(result) == 0 or result[-1] != b[j]:
+                    result.append(b[j])
+                j += 1
+                
+        while i < n:
+            if len(result) == 0 or result[-1] != a[i]:
+                result.append(a[i])
+            i += 1
+            
+        while j < m:
+            if len(result) == 0 or result[-1] != b[j]:
+                result.append(b[j])
+            j += 1
+            
+        return result
+	
+#########################################################################################
