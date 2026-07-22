a8d5a103b658c8fef3e4e8e93a035c738364fd78
Milan Pansuriya <milanpansuriya1234@gmail.com>
2026-07-22 19:56:20 +0530

    Add optimized implementation for finding all unique quadruplets that sum to a target

--- Python_Coding_Challange/4 Sum.py
+++ Python_Coding_Challange/4 Sum.py
@@ -28,3 +28,43 @@ class Solution:
         return result
 
 #########################################################################################
+#--Optimized Approach--#
+
+class Solution:
+    def fourSum(self, arr, target):
+        # code here
+        
+        n = len(arr)
+        arr.sort()
+        result = []
+        
+        for i in range(0,n):
+            if i > 0 and arr[i] == arr[i-1]:
+                continue
+            
+            for j in range(i+1,n):
+                if j > i+1 and arr[j] == arr[j-1]:
+                    continue
+                
+                k = j + 1
+                l = n - 1
+                
+                while k < l:
+                    total = arr[i] + arr[j] + arr[k] + arr[l]
+                    if total == target:
+                        temp = [arr[i], arr[j], arr[k], arr[l]]
+                        result.append(temp)
+                        k += 1
+                        l -= 1
+                        
+                        while k < l and arr[k] == arr[k-1]:
+                            k += 1
+                        while l > k and arr[l] == arr[l+1]:
+                            l -= 1
+                    elif total < target:
+                        k += 1
+                    else:
+                        l -= 1
+        return result
+    
+#########################################################################################
