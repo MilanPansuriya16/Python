c807557cfbb4f8200b9cb0fc1ee98910ba1a99ef
Milan Pansuriya <milanpansuriya1234@gmail.com>
2026-08-24 14:51:48 +0530

    Add implementation for checking subsequences with a sum equal to K

---
+++ Python_Coding_Challange/All subsequences with sum equals to K.py
@@ -0,0 +1,30 @@
+
+#########################################################################################
+
+class Solution:
+    def checkSubsequenceSum(self, arr, k):
+        # code here
+        
+        result = []
+        
+        def backtrack(index,total,subset):
+            if total == k:
+                result.append(subset.copy())
+                return 
+            elif total > k:
+                return
+            if index >= len(arr):
+                return 
+        
+            subset.append(arr[index])
+            new_sum = total + arr[index]
+            backtrack(index+1,new_sum,subset)
+            e = subset.pop()
+            new_sum -= e
+            backtrack(index+1,new_sum,subset)
+            
+        backtrack(0,0,[])
+        
+        return result
+
+#########################################################################################
