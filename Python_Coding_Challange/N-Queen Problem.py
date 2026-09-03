8932bd9365309a156ebf7bbd7c8c545064f5395a
Milan Pansuriya <milanpansuriya1234@gmail.com>
2026-09-03 15:31:07 +0530

    Add optimized solution for N-Queen problem using backtracking

--- Python_Coding_Challange/N-Queen Problem.py
+++ Python_Coding_Challange/N-Queen Problem.py
@@ -115,4 +115,43 @@ class Solution:
         
         return ans
             
-#########################################################################################              
+#########################################################################################  
+########## Optimized GFG Solution ##########
+#########################################################################################            
+class Solution:
+        
+    def solve(self,col,board,ans,leftrow,upperDiagonal,lowerDiagonal,temp,n):
+        if col == n:
+            ans.append(temp[:])
+            return
+        
+        for row in range(n):
+            if ( 
+                leftrow[row] == 0 and
+                lowerDiagonal[row + col] == 0 and
+                upperDiagonal[n - 1 + col - row] == 0
+            ):
+                board[row] = board[row][:col] + "Q" + board[row][col+1:]
+                leftrow[row] = 1
+                lowerDiagonal[row+ col] = 1
+                upperDiagonal[n - 1 + col - row] = 1
+                temp.append(row + 1) # GFG uses 1-based indexing
+                
+                self.solve(col+1,board,ans,leftrow,upperDiagonal,lowerDiagonal,temp,n)
+                temp.pop()
+                board[row] = board[row][:col] + "." +board[row][col+1:]
+                leftrow[row] = 0
+                lowerDiagonal[row+ col] = 0
+                upperDiagonal[n - 1 + col - row] = -0
+        
+    def nQueen(self, n: int) -> list[list[int]]:
+        # code here
+        ans = []
+        board = ["."*n for i in range(n)]
+        leftrow = [0] * n
+        upperDiagonal = [0] * (2 * n -1)
+        lowerDiagonal = [0] * (2 * n -1)
+        
+        self.solve(0,board,ans,leftrow,upperDiagonal,lowerDiagonal,[],n)
+        
+        return ans
