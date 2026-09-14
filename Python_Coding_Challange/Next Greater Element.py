69c1d7cc8df8ed4c5b2e916d4d6555b7b8110476
Milan Pansuriya <milanpansuriya1234@gmail.com>
2026-09-14 16:17:15 +0530

    Add implementation of Next Greater Element using stack

---
+++ Python_Coding_Challange/Next Greater Element.py
@@ -0,0 +1,26 @@
+'''
+https://www.geeksforgeeks.org/problems/next-larger-element-1587115620/1
+'''
+
+
+#########################################################################################
+########################    Stack based solution    ########################
+
+class Solution:
+    def nextLargerElement(self, arr):
+        # code here
+        n = len(arr)
+        ans = [-1] * n
+        stack = []
+        
+        for i in range(n-1,-1,-1):
+            while len(stack) != 0 and stack[-1] <= arr[i]:
+                stack.pop()
+            if stack:
+                ans[i] = stack[-1]
+            
+            stack.append(arr[i])
+        
+        return ans
+	
+#########################################################################################
--- Test Python code.py
+++ Test Python code.py
@@ -1,3 +1,4 @@
-n = 4
-board = ["."*n for i in range(n)]
-print(board)
+
+arr = [1,2,3,4,5]
+for i in range(len(arr)-1,-1,-1):
+    print(i)
